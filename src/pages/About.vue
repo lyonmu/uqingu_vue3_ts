67923dc3b449d145a440e12db56e123da1aacbfa
uqingu <uqingu@foxmail.com>
2022-07-01 18:11:22 +0800

    test bytedance/bytemd.js

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,5 +1,6 @@
 <template>
     <md-editor class="mdclass" v-model="aboutMe" :previewOnly="previewState" :showCodeRowNumber="previewState" previewTheme="mk-cute" />
+    <!-- <Editor class="bytemd" :value="aboutMe" :plugins="plugins" mode="split"/> -->
 </template>
 
 <script lang="ts" setup>
@@ -7,6 +8,14 @@ import { useStore } from '../stores'
 import { onMounted, toRefs } from "vue"
 import MdEditor from 'md-editor-v3';
 import 'md-editor-v3/lib/style.css';
+/* import { Editor, Viewer } from '@bytemd/vue-next'
+import 'bytemd/dist/index.css'
+import gfm from '@bytemd/plugin-gfm' */
+
+/* const plugins = [
+  gfm(),
+  // Add more plugins here
+] */
 
 const store = useStore()
 
@@ -25,5 +34,8 @@ onMounted(() => {
     width: 1200px;
     /* border: 2px inset #3b2e7e; */
 }
+/* .bytemd {
+  height: 100%;
+} */
 </style>
 
